f3e1b875275b51d383f1f8d2236d796bec64b1c4
MaroonShaded <51722734+MaroonShaded@users.noreply.github.com>
2020-01-11 11:18:31 +0000

    Add src folder

---
+++ src/main/java/rovetown/testmod/TestMod.java
@@ -0,0 +1,32 @@
+package rovetown.testmod;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(TestMod.MODID)
+public class TestMod
+{
+	public static final String MODID = "testmod";
+	public static final Logger LOGGER = LogManager.getLogger(MODID);
+	
+	public TestMod()
+	{
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientRegistries);
+	}
+	
+	private void setup(final FMLCommonSetupEvent event)
+	{
+		LOGGER.info("Setup method registered.");
+	}
+	
+	private void clientRegistries(final FMLClientSetupEvent event)
+	{
+		LOGGER.info("clientRegistries method registered.");
+	}
+}
---
+++ src/main/java/rovetown/testmod/init/ModItems.java
@@ -0,0 +1,6 @@
+package rovetown.testmod.init;
+
+public class ModItems
+{
+	// public static final Item STEEL_INGOT;
+}
